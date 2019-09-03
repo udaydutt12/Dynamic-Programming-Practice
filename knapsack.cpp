1b66ac1d597d3821002a86700b2ff1ec1048150b
udutt <uday.dutt@ansys.com>
2019-09-03 11:22:44 -0500

    Correcting the knapsack

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <utility> 
 #include <vector>
+#include <algorithm>
 
-int Knapsack(std::vector <std::pair<int,int>> items, int totalweight)
+int Knapsack(std::vector <std::pair<int,int> > items, int totalweight)
 {
     int rows = items.size();
     int cols = totalweight+1;
     //1. Create an array of size [items.length()]*[Totalweight+1] all set to 0
-    std::vector <std::vector <int>> matrix(rows,std::vector<int> (cols));
-    std::pair ptr = {0,0};
-    std::pair item;
-    int prev = 0;
-    while (ptr.first<rows)
+    std::vector <std::vector <int> > matrix(rows,std::vector<int> (cols,0));
+    std::pair<int,int> item;
+    for (int i=0;i<rows;++i)
     {
-        item = items[ptr.first];
-        while (ptr.second < cols)
+        item = items[i];
+        for (int j=1;j<cols;++j)
         {
-            if (ptr.second < item.second)
-            {
-                matrix[ptr.first][ptr.second] = prev;
-            }
+            if (j < item.second)
+                matrix[i][j] = matrix[i-1][j];
             else
             {
-                matrix[ptr.first][ptr.second] = std::max(item.first+matrix[ptr.first-1][ptr.second-item.second],prev);
-                prev = matrix[ptr.first][ptr.second];
+                if (i==0) 
+                    matrix[i][j]=item.second;
+                else
+                    {
+                        matrix[i][j] = std::max(item.first+matrix[i-1][j-item.second],matrix[i-1][j]);
+                    }
             }
-            ++ptr.second;
         }
-        prev=0;
-        ++ptr.first;
+    }
+    for (auto x:matrix)
+    {
+        for (int y:x)
+            std::cout<<y<<" ";
+        std::cout<<std::endl;
     }
     return matrix.back().back();
 }
@@ -36,11 +40,12 @@ int Knapsack(std::vector <std::pair<int,int>> items, int totalweight)
 
 int main()
 {
-    std::vector <std::pair<int,int>> items;
-    items.push_back({1,1});
-    items.push_back({3,4});
-    items.push_back({4,5});
-    items.push_back({5,7});
+    std::vector < std::pair<int,int> > items;
+    items.push_back(std::make_pair(1,1));
+    items.push_back(std::make_pair(4,3));
+    items.push_back(std::make_pair(5,4));
+    items.push_back(std::make_pair(7,5));
     int totalweight=7;
     int maxval = Knapsack(items,totalweight);
+    std::cout<<maxval;
 }
